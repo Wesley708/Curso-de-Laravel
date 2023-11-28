99181f8ca93041c2491c675c2ec86f656b20473f
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2023-11-28 19:08:56 -0300

    For e Foreach no Láravel

--- htcevents/resources/views/welcome.blade.php
+++ htcevents/resources/views/welcome.blade.php
@@ -27,5 +27,22 @@
         <p>O nome é {{$nome}}</p>
         <p>E ele tem {{$idade}} anos!</p>
         @endif
+        @for($i = 0; $i < count($arr); $i++)
+            <p>{{$arr[$i]}} - {{$i}}</p>
+            @if($i == 2)
+                <p>O número é {{$arr[$i]}}</p>
+            @endif
+        @endfor
+        @php
+            $name = "João";
+            echo $name;
+        @endphp
+        {{-- Este é um comentário do Blade! --}}
+        @foreach ($nomes as $nome)
+            <h1>
+                {{$loop->index}}
+               O nome é {{$nome}}
+            </h1>
+        @endforeach
     </body>
 </html>
--- htcevents/routes/web.php
+++ htcevents/routes/web.php
@@ -14,9 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $nomes = ["Wesley", "Mariana", "Marlene", "Fulano"];
+    $arr = [10, 20, 30, 40, 50];
     $nome = "Wesley";
     $idade = 25;
-    return view('welcome', ['nome' => $nome , 'idade' => $idade]);
+
+    return view('welcome',
+        [
+            'nome' => $nome,
+            'idade' => $idade,
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]
+    );
 });
 
 Route::get('/contact', function () {
