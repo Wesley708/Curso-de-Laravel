367577c07e5bffc4ba0974cd11cdc666921ca738
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2023-11-28 18:08:49 -0300

    Conhecendo o Blade

--- htcevents/routes/web.php
+++ htcevents/routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Wesley";
+    $idade = 25;
+    return view('welcome', ['nome' => $nome , 'idade' => $idade]);
 });
 
 Route::get('/contact', function () {
