89212d9ee79707d8bfb62caefbe7f4387b054e39
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2024-01-01 23:48:43 -0300

    Página de dados do evento

--- htcevents/resources/views/welcome.blade.php
+++ htcevents/resources/views/welcome.blade.php
@@ -5,13 +5,13 @@
 @section('content')
 
    <div id="search-container" class="col-md-12">
-    <h1>Busque o Evento</h1>
-    <form action="">
-        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+        <h1>Busque o Evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
 
-    </form>
+        </form>
    </div>
-   <div id="events-container" class="col-md-12">
+   <div id="events-container" class="">
         <h2>Próximos eventos</h2>
         <p class="subtitle">Veja os eventos dos próximos dias</p>
         <div id="cards-container" class="row">
@@ -22,7 +22,7 @@
                         <p class="card-date">25/12/2023</p>
                         <h5 class="card-title">{{$event->title}}</h5>
                         <p class="card-participants">X Participantes</p>
-                        <a href="#" class="btn btn-primary">Saber mais</a>
+                        <a href="/events/{{ $event->id }}" class="btn btn-primary">Saiba mais</a>
                     </div>
                 </div>
             @endforeach
--- htcevents/routes/web.php
+++ htcevents/routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\SomaController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/soma', [SomaController::class, 'soma']);
