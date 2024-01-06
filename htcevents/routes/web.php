5f217a4385c86ad6d1251cf26b5e43f211bbb2df
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2024-01-06 17:42:39 -0300

    Autenticação usando Livewire e jetstream

--- htcevents/routes/web.php
+++ htcevents/routes/web.php
@@ -26,3 +26,13 @@ Route::get('/soma', [SomaController::class, 'soma']);
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
